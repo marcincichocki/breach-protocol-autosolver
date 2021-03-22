90894ca68661edac4c35f914176c46bda847bef3
Marcin Cichocki <marcincichocki1993@gmail.com>
2021-03-22 18:03:40 +0100

    update screenshot-desktop typings

--- types/screenshot-desktop/index.d.ts
+++ types/screenshot-desktop/index.d.ts
@@ -3,11 +3,35 @@ declare module 'screenshot-desktop' {
     filename?: string;
     format?: string;
     linuxLibrary?: 'scrot' | 'imagemagick';
+    screen?: string;
+  }
+
+  interface ScreenshotDisplayOutput {
+    id: string;
+    name: string;
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+    dpiScale: number;
+    width: number;
+    height: number;
   }
 
   function screenshot(
     options?: ScreenshotOptions
   ): Promise<Buffer> | Promise<string>;
+
+  namespace screenshot {
+    function listDisplays(): Promise<ScreenshotDisplayOutput>;
+    function availableDisplays(): Promise<ScreenshotDisplayOutput>;
+
+    function parseDisplayOutput(output: string): ScreenshotDisplayOutput;
+
+    const EXAMPLE_DISPLAYS_OUTPUT: string;
+
+    function all(): Promise<Buffer[]> | Promise<string[]>;
+  }
 
   export = screenshot;
 }
