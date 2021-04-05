3c65c9469169333f22c5cab8a1ba35f3fba532be
Marcin Cichocki <marcincichocki1993@gmail.com>
2021-04-05 15:59:42 +0200

    add error handling to null path

--- src/platform-node/solve.ts
+++ src/platform-node/solve.ts
@@ -20,11 +20,43 @@ export async function solveBreachProtocol(
 ) {
   const log = ora(t`CAPTURE_SCREEN ${screenId}`).start();
   const fileName = (await captureScreen(screenId)) as string;
-  let ocr = null;
+  const ocr = await tryToOCRBreachProtocol(fileName, workers, log);
 
+  // exit early because data is invalid.
+  if (!ocr) return;
+
+  await remove(fileName);
+  log.text = t`SOLVER_START`;
+
+  const data = transformRawData(ocr.rawData);
+  const sequences = makeSequences(data.daemons, data.bufferSize);
+  const game = new BreachProtocol(data.tGrid, data.bufferSize);
+  const result = game.solve(sequences);
+
+  if (!result) {
+    if (!options.disableSound) {
+      play(options.soundPath);
+    }
+
+    log.fail(t`NO_SOLUTION`);
+
+    return;
+  }
+
+  await resolveBreachProtocol(result.path, ocr.squarePositionMap);
+
+  log.succeed(t`SOLVER_DONE`);
+}
+
+function tryToOCRBreachProtocol(
+  fileName: string,
+  workers: Record<FragmentId, Tesseract.Worker>,
+  log: ora.Ora
+) {
   try {
     log.text = t`OCR_START`;
-    ocr = await breachProtocolOCR(fileName, workers);
+
+    return breachProtocolOCR(fileName, workers);
   } catch (e) {
     if (e instanceof BreachProtocolValidationError) {
       if (!options.disableSound) {
@@ -36,23 +68,9 @@ export async function solveBreachProtocol(
       appendToDebugJson(debugData);
 
       log.fail(e.message);
-
-      // exit early because data is invalid.
       return;
     }
 
     throw e;
   }
-
-  await remove(fileName);
-  log.text = t`SOLVER_START`;
-
-  const data = transformRawData(ocr.rawData);
-  const sequences = makeSequences(data.daemons, data.bufferSize);
-  const game = new BreachProtocol(data.tGrid, data.bufferSize);
-  const result = game.solve(sequences);
-
-  await resolveBreachProtocol(result.path, ocr.squarePositionMap);
-
-  log.succeed(t`SOLVER_DONE`);
 }
