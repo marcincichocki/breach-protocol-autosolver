757ecdb2a1b16806fb9cf07d722d661a8dd10dc6
Marcin Cichocki <marcincichocki1993@gmail.com>
2021-04-15 13:22:26 +0200

    refactor ocr test

--- src/core/ocr.test.ts
+++ src/core/ocr.test.ts
@@ -1,14 +1,11 @@
 import path from 'path';
+import sharp from 'sharp';
 import registry from '../bp-registry/registry.json';
 import { BufferSize } from './common';
-import configs from './configs.json';
-import {
-  BreachProtocolFragmentConfig,
-  breachProtocolOCR,
-  FragmentId,
-  loadWorkers,
-  Resolution,
-} from './ocr';
+import { Resolution } from './ocr';
+import { BreachProtocolOCRFragment } from './ocr/base';
+import { SharpImageContainer } from './ocr/image-container';
+import { breachProtocolOCR2 } from './ocr/ocr';
 
 interface RegistryEntry {
   fileName: string;
@@ -20,43 +17,47 @@ interface RegistryEntry {
 type Registry = Record<Resolution, RegistryEntry[]>;
 
 describe('ocr', () => {
-  let workers: Record<FragmentId, Tesseract.Worker>;
-
   beforeAll(async () => {
-    workers = await loadWorkers(configs as BreachProtocolFragmentConfig[]);
+    await BreachProtocolOCRFragment.initScheduler();
   }, 30000);
 
   afterAll(async () => {
-    for (const fragmentId in workers) {
-      const worker = workers[fragmentId as FragmentId];
+    await BreachProtocolOCRFragment.terminateScheduler();
+  });
 
-      await worker.terminate();
+  it.each(getRegistryFor('1920x1080'))(
+    'should correctly ocr 1920x1080/%s',
+    async (f: string, entry: RegistryEntry) => {
+      await compareOcrToJson(entry, '1920x1080');
     }
-  });
+  );
 
-  it('should correctly ocr 1920x1080 images', async () => {
-    await compareOcrToJson('1920x1080', workers);
-  });
+  it.each(getRegistryFor('2560x1440'))(
+    'should correctly ocr 2560x1440/%s',
+    async (f: string, entry: RegistryEntry) => {
+      await compareOcrToJson(entry, '2560x1440');
+    }
+  );
 
-  it('should correctly ocr 2560x1440 images', async () => {
-    await compareOcrToJson('2560x1440', workers);
-  });
-
-  it('should correctly ocr 3840x2160 images', async () => {
-    await compareOcrToJson('3840x2160', workers);
-  });
+  it.each(getRegistryFor('3840x2160'))(
+    'should correctly ocr 3840x2160/%s',
+    async (f: string, entry: RegistryEntry) => {
+      await compareOcrToJson(entry, '3840x2160');
+    }
+  );
 });
 
-async function compareOcrToJson(
-  resolution: Resolution,
-  workers: Record<FragmentId, Tesseract.Worker>
-) {
-  for (const entry of (registry as Registry)[resolution]) {
-    const file = path.join('./src/bp-registry', resolution, entry.fileName);
-    const { rawData } = await breachProtocolOCR(file, workers);
+function getRegistryFor(resolution: Resolution) {
+  return (registry as Registry)[resolution].map((e) => [e.fileName, e]);
+}
 
-    expect(rawData.grid).toEqual(entry.grid);
-    expect(rawData.daemons).toEqual(entry.daemons);
-    expect(rawData.bufferSize).toBe(entry.bufferSize);
-  }
+async function compareOcrToJson(entry: RegistryEntry, resolution: Resolution) {
+  const file = path.join('./src/bp-registry', resolution, entry.fileName);
+  const image = sharp(file);
+  const container = await SharpImageContainer.create(image);
+  const { rawData } = await breachProtocolOCR2(container);
+
+  expect(rawData.grid).toEqual(entry.grid);
+  expect(rawData.daemons).toEqual(entry.daemons);
+  expect(rawData.bufferSize).toBe(entry.bufferSize);
 }
