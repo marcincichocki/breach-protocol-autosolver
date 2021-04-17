126ede31e858f78d26730c746fcbd6a4cc435923
Marcin Cichocki <marcincichocki1993@gmail.com>
2021-04-17 17:27:57 +0200

    make screenshot

--- src/client-electron/worker/index.ts
+++ src/client-electron/worker/index.ts
@@ -1,21 +1,32 @@
 import { BreachProtocolOCRFragment } from '@/core';
+import { captureScreen } from '@/platform-node/robot';
 import { ipcRenderer } from 'electron';
+import { listDisplays } from 'screenshot-desktop';
 
 function log(message: any) {
   ipcRenderer.send('log', message);
 }
 
+let screen: string;
+
 async function init() {
   log('STARTING INIT');
 
   await BreachProtocolOCRFragment.initScheduler();
+  const displays = await listDisplays();
+  screen = displays[0].id;
+
+  console.log(displays);
 
   ipcRenderer.send('scheduler-ready');
 }
 
 init();
 
-ipcRenderer.on('start-autosolver', () => {
+ipcRenderer.on('start-autosolver', async () => {
+  log('starting autosolver!!!!');
+
+  const fileName = await captureScreen(screen);
   // do some expensive computations...
 
   ipcRenderer.send('autosolver-finished');
