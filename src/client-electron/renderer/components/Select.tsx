aee4df15b952a23bd0bd308b5bc0a21e4fe49af6
Marcin Cichocki <marcincichocki1993@gmail.com>
2021-05-03 00:45:45 +0200

    feat(client-electron): add form controls
    fixes #69

---
+++ src/client-electron/renderer/components/Select.tsx
@@ -0,0 +1,131 @@
+import { FC, HTMLProps, useRef, useState } from 'react';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+import styled from 'styled-components';
+
+const SelectWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 510px;
+  height: 40px;
+  background: var(--background);
+  border: 1px solid var(--primary-dark);
+  box-sizing: border-box;
+  padding: 0 2rem;
+
+  &:hover {
+    background: var(--primary-darker);
+    border: 1px solid var(--accent);
+  }
+`;
+
+const SelectButton = styled.button`
+  border: none;
+  background: none;
+  color: var(--accent);
+  font-size: 2.5em;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  outline: none;
+  cursor: pointer;
+`;
+
+const SelectViewerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SelectViewerValue = styled.span`
+  color: var(--accent);
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 1.1;
+`;
+
+const SelectViewerOptions = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const SelectViewerOption = styled.div<{ active: boolean }>`
+  width: 40px;
+  height: 4px;
+  background: ${({ active }) => (active ? 'var(--primary)' : '#411518')};
+`;
+
+interface SelectOption {
+  value: string;
+  name: string;
+}
+
+interface SelectViewerProps {
+  options: SelectOption[];
+  index: number;
+}
+
+const SelectViewer: FC<SelectViewerProps> = ({ options, index }) => (
+  <SelectViewerWrapper>
+    <SelectViewerValue>{options[index].name}</SelectViewerValue>
+    <SelectViewerOptions>
+      {options.map((option, i) => (
+        <SelectViewerOption key={i} active={index === i}></SelectViewerOption>
+      ))}
+    </SelectViewerOptions>
+  </SelectViewerWrapper>
+);
+
+interface SelectProps extends HTMLProps<HTMLSelectElement> {
+  options: SelectOption[];
+}
+
+export const Select: FC<SelectProps> = ({ options, value, ...props }) => {
+  const ref = useRef<HTMLSelectElement>();
+  const [index, setIndex] = useState<number>(
+    value ? options.findIndex((o) => o.value === value.toString()) : 0
+  );
+
+  const dispatchChangeEvent = () => {
+    const event = new Event('change', { bubbles: true });
+    ref.current.dispatchEvent(event);
+  };
+
+  const selectOption = (index: number) => {
+    ref.current.selectedIndex = index;
+    setIndex(ref.current.selectedIndex);
+  };
+
+  const prev = () => {
+    if (!ref.current.selectedIndex) return;
+
+    selectOption(ref.current.selectedIndex - 1);
+    dispatchChangeEvent();
+  };
+
+  const next = () => {
+    if (ref.current.selectedIndex === options.length - 1) return;
+
+    selectOption(ref.current.selectedIndex + 1);
+    dispatchChangeEvent();
+  };
+
+  return (
+    <SelectWrapper>
+      <select ref={ref} defaultValue={value} {...props} hidden>
+        {options.map(({ name, value }) => (
+          <option key={value} value={value}>
+            {name}
+          </option>
+        ))}
+      </select>
+      <SelectButton onClick={prev}>
+        <MdKeyboardArrowLeft />
+      </SelectButton>
+      <SelectViewer options={options} index={index} />
+      <SelectButton onClick={next}>
+        <MdKeyboardArrowRight />
+      </SelectButton>
+    </SelectWrapper>
+  );
+};
--- src/client-electron/renderer/styles/global.tsx
+++ src/client-electron/renderer/styles/global.tsx
@@ -6,13 +6,17 @@ export const GlobalStyles = createGlobalStyle`
 
   :root {
     --primary: #ff5851;
-    --background: #121018;
+    --primary-dark: #701d1f;
+    --primary-darker: #6f2223;
+    /* --background: #121018; */
+    --background: #161220;
     --accent: #5FF6FF;
   }
 
   html,
   body {
     height: 100%;
+    max-height: 100%;
     font-family: Rajdhani;
   }
 
@@ -25,5 +29,17 @@ export const GlobalStyles = createGlobalStyle`
     display: flex;
     flex-direction: column;
     height: 100%;
+  }
+
+  ::-webkit-scrollbar {
+    width: 6px;
+  }
+  
+  ::-webkit-scrollbar-track {
+    background: var(--primary-darker)
+  }
+  
+  ::-webkit-scrollbar-thumb {
+    background: var(--primary);
   }
 `;
