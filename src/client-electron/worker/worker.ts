7fe3b13f5a2d4543665acfb0a0e90ae3d33ceecc
Marcin Cichocki <marcincichocki1993@gmail.com>
2021-05-16 22:49:52 +0200

    refactor worker

--- src/client-electron/worker/worker.ts
+++ src/client-electron/worker/worker.ts
@@ -4,88 +4,131 @@ import {
   BreachProtocolDaemonsFragment,
   BreachProtocolGridFragment,
   BreachProtocolOCRFragment,
-  FragmentId,
   SharpImageContainer,
 } from '@/core';
-import { ipcRenderer as ipc } from 'electron';
-import { listDisplays } from 'screenshot-desktop';
+import { ipcRenderer as ipc, IpcRendererEvent } from 'electron';
+import { listDisplays, ScreenshotDisplayOutput } from 'screenshot-desktop';
 import sharp from 'sharp';
 import {
   Action,
   Request,
   TestThresholdData,
-  workerListener,
+  workerAsyncRequestListener,
   WorkerStatus,
 } from '../common';
 import { BreachProtocolAutosolver } from './autosolver';
 
-function updateStatus(payload: WorkerStatus) {
-  dispatch({ type: 'SET_STATUS', payload });
-}
+class BreachProtocolWorker {
+  private activeDisplayId: string = null;
 
-function dispatch(action: Omit<Action, 'origin'>) {
-  ipc.send('state', { ...action, origin: 'worker' });
-}
+  private disposeAsyncRequestListener: () => void = null;
 
-ipc.on('SET_ACTIVE_DISPLAY', (event, state) => {
-  screenId = state.id;
-});
+  private fragments: {
+    grid: BreachProtocolGridFragment<sharp.Sharp>;
+    daemons: BreachProtocolDaemonsFragment<sharp.Sharp>;
+    bufferSize: BreachProtocolBufferSizeFragment<sharp.Sharp>;
+  } = null;
 
-const disposeAsyncRequestListener = workerListener(handleAsyncRequest);
+  async bootstrap() {
+    this.registerListeners();
 
-async function handleAsyncRequest(req: Request) {
-  switch (req.type) {
-    case 'TEST_THRESHOLD':
-      return testThreshold(req);
-    default:
+    this.updateStatus(WorkerStatus.Bootstrap);
+
+    // TODO: change lang, or remove it completly.
+    setLang('en');
+    const displays = await listDisplays();
+    this.activeDisplayId = displays[0].id;
+
+    this.dispatch({ type: 'SET_DISPLAYS', payload: displays });
+    this.dispatch({ type: 'SET_ACTIVE_DISPLAY', payload: displays[0] });
+
+    await BreachProtocolOCRFragment.initScheduler();
+
+    this.updateStatus(WorkerStatus.Ready);
+    ipc.send('worker:ready');
+  }
+
+  dispose() {
+    ipc.removeAllListeners('worker:solve');
+    ipc.removeAllListeners('SET_ACTIVE_DISPLAY');
+
+    this.disposeAsyncRequestListener();
+    this.disposeTestThreshold();
+  }
+
+  private registerListeners() {
+    ipc.on('worker:solve', this.onWorkerSolve.bind(this));
+    ipc.on('SET_ACTIVE_DISPLAY', this.onSetActiveDisplay.bind(this));
+
+    this.disposeAsyncRequestListener = workerAsyncRequestListener(
+      this.handleAsyncRequest.bind(this)
+    );
+  }
+
+  private async onWorkerSolve() {
+    this.updateStatus(WorkerStatus.Working);
+
+    const bpa = new BreachProtocolAutosolver(this.activeDisplayId);
+    await bpa.solve();
+
+    this.dispatch({ type: 'ADD_HISTORY_ENTRY', payload: bpa.toJSON() });
+    this.updateStatus(WorkerStatus.Ready);
+  }
+
+  private onSetActiveDisplay(
+    e: IpcRendererEvent,
+    display: ScreenshotDisplayOutput
+  ) {
+    this.activeDisplayId = display.id;
+  }
+
+  private async handleAsyncRequest(req: Request) {
+    switch (req.type) {
+      case 'TEST_THRESHOLD_INIT':
+        return this.initTestThreshold(req);
+      case 'TEST_THRESHOLD_DISPOSE':
+        return this.disposeTestThreshold();
+      case 'TEST_THRESHOLD':
+        return this.testThreshold(req);
+      default:
+    }
+  }
+
+  private async initTestThreshold(req: Request<string>) {
+    const instance = sharp(req.data);
+    const container = await SharpImageContainer.create(instance);
+
+    this.fragments = {
+      grid: new BreachProtocolGridFragment(container),
+      daemons: new BreachProtocolDaemonsFragment(container),
+      bufferSize: new BreachProtocolBufferSizeFragment(container),
+    };
+  }
+
+  private disposeTestThreshold() {
+    this.fragments = null;
+  }
+
+  private async testThreshold(req: Request<TestThresholdData>) {
+    const fragment = this.fragments[req.data.fragmentId];
+
+    return fragment.recognize(req.data.threshold, false);
+  }
+
+  private updateStatus(payload: WorkerStatus) {
+    this.dispatch({ type: 'SET_STATUS', payload });
+  }
+
+  private dispatch(action: Omit<Action, 'origin'>) {
+    ipc.send('state', { ...action, origin: 'worker' });
   }
 }
 
-function getFragment(id: FragmentId, container: SharpImageContainer) {
-  switch (id) {
-    case 'grid':
-      return new BreachProtocolGridFragment(container);
-    case 'daemons':
-      return new BreachProtocolDaemonsFragment(container);
-    case 'bufferSize':
-      return new BreachProtocolBufferSizeFragment(container);
-  }
-}
+const worker = new BreachProtocolWorker();
 
-async function testThreshold(req: Request<TestThresholdData>) {
-  const instance = sharp(req.data.fileName);
-  const container = await SharpImageContainer.create(instance);
-  const fragment = getFragment(req.data.fragmentId, container);
+// TODO: error handling in worker.
+worker.bootstrap();
 
-  return fragment.recognize(req.data.threshold, false);
-}
-
-let screenId: string = null;
-
-async function bootstrap() {
-  updateStatus(WorkerStatus.Bootstrap);
-
-  setLang('en');
-  const displays = await listDisplays();
-  screenId = displays[0].id;
-
-  dispatch({ type: 'SET_DISPLAYS', payload: displays });
-  dispatch({ type: 'SET_ACTIVE_DISPLAY', payload: displays[0] });
-
-  await BreachProtocolOCRFragment.initScheduler();
-
-  ipc.send('worker:ready');
-  updateStatus(WorkerStatus.Ready);
-}
-
-bootstrap();
-
-ipc.on('worker:solve', async () => {
-  updateStatus(WorkerStatus.Working);
-
-  const bpa = new BreachProtocolAutosolver(screenId);
-  await bpa.solve();
-
-  dispatch({ type: 'ADD_HISTORY_ENTRY', payload: bpa.toJSON() });
-  updateStatus(WorkerStatus.Ready);
+window.addEventListener('unload', () => {
+  worker.dispose();
 });
