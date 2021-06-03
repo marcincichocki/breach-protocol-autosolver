0ad0ae81c95947843e04ad99b6b0a005555d93f4
Marcin Cichocki <marcincichocki1993@gmail.com>
2021-06-03 22:03:46 +0200

    display link button in details

--- src/client-electron/renderer/app.tsx
+++ src/client-electron/renderer/app.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { useIpcEvent, useIpcState } from './common';
