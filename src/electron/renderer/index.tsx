a2970f700d741a438e80350e414cdc9cf4f1dfcf
Marcin Cichocki <marcincichocki1993@gmail.com>
2021-06-16 19:50:38 +0200

    remove prefixes

--- src/electron/renderer/index.tsx
+++ src/electron/renderer/index.tsx
@@ -1,14 +1,15 @@
 import { render } from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
+import { StyleSheetManager } from 'styled-components';
 import { App } from './app';
 import { GlobalStyles } from './styles/global';
 
 render(
-  <>
-    <GlobalStyles />
+  <StyleSheetManager disableVendorPrefixes>
     <Router>
+      <GlobalStyles />
       <App />
     </Router>
-  </>,
+  </StyleSheetManager>,
   document.getElementById('root')
 );
