e88c17157a67eeade7a7d4d8aee4d2e53894d21d
Marcin Cichocki <marcincichocki1993@gmail.com>
2021-07-09 11:40:49 +0200

    refactor(client-electron): update options configuration

--- src/electron/common/options.ts
+++ src/electron/common/options.ts
@@ -1,140 +1,132 @@
 import { AppSettings } from './common';
 
-class BreachProtocolOption<T = any> {
-  constructor(
-    public readonly name: string,
-    public readonly description: string,
-    public readonly defaultValue: T
-  ) {}
+interface BreachProtocolOption {
+  id: string;
+  description: string;
+  defaultValue: boolean | string | number;
 }
 
-const options = [
-  new BreachProtocolOption<string>(
-    'activeDisplayId',
-    'Select monitor on which Cyberpunk 2077 is running.',
-    null
-  ),
-  new BreachProtocolOption(
-    'autoUpdate',
-    'Update autosolver automatically.',
-    true
-  ),
-  new BreachProtocolOption(
-    'delay',
-    'Delay in milliseconds between output clicks.',
-    75
-  ),
-  new BreachProtocolOption(
-    'autoExit',
-    'Automatically exit breach protocol after solving it.',
-    true
-  ),
-  new BreachProtocolOption(
-    'soundEnabled',
-    'Disable/enable sound effects.',
-    true
-  ),
-  new BreachProtocolOption(
-    'experimentalBufferSizeRecognition',
-    'Use experimental buffer size recognition. Recommended for very high in game gamma(above 1.5).',
-    false
-  ),
-  new BreachProtocolOption(
-    'format',
-    'Format in which save source images. "png" offers better quality, but takes more space on disk.',
-    'png'
-  ),
-  new BreachProtocolOption(
-    'historySize',
-    'Amount of history entires saved.',
-    3
-  ),
-  new BreachProtocolOption(
-    'keyBind',
-    'Key bind which triggers autosolver. Press "Enter" to save new key bind, or "Escape" to cancel.',
-    'Alt+`'
-  ),
-  new BreachProtocolOption(
-    'preserveSourceOnSuccess',
-    'Preserve image source after successful breach protocol. This might take a lot of space depending on format and history size options.',
-    true
-  ),
-  new BreachProtocolOption(
-    'checkForUpdates',
-    'Check for updates on startup.',
-    true
-  ),
-  new BreachProtocolOption(
-    'errorSoundPath',
-    'Path to error sound.',
-    'C:/Windows/Media/Windows Foreground.wav'
-  ),
-  new BreachProtocolOption(
-    'thresholdBufferSize',
-    'Fixed threshold value for buffer size fragment.',
-    127
-  ),
-  new BreachProtocolOption(
-    'thresholdBufferSizeAuto',
-    'Use automatic threshold for buffer size fragment.',
-    true
-  ),
-  new BreachProtocolOption(
-    'thresholdDaemons',
-    'Fixed threshold value for daemons fragment.',
-    127
-  ),
-  new BreachProtocolOption(
-    'thresholdDaemonsAuto',
-    'Use automatic threshold for daemons fragment.',
-    true
-  ),
-  new BreachProtocolOption(
-    'thresholdGrid',
-    'Fixed threshold value for grid fragment.',
-    127
-  ),
-  new BreachProtocolOption(
-    'thresholdGridAuto',
-    'Use automatic threshold for grid fragment.',
-    true
-  ),
-  new BreachProtocolOption(
-    'useScaling',
-    'Use Windows scaling to calculate coordinates of squares.',
-    false
-  ),
-  new BreachProtocolOption(
-    'minimizeToTray',
-    'Minimize app to system tray.',
-    false
-  ),
-  new BreachProtocolOption(
-    'outputDevice',
-    'Output device that will be used to solve breach protocol.',
-    'mouse'
-  ),
+const options: BreachProtocolOption[] = [
+  {
+    id: 'activeDisplayId',
+    description: 'Select monitor on which Cyberpunk 2077 is running.',
+    defaultValue: null,
+  },
+  {
+    id: 'autoUpdate',
+    description: 'Update autosolver automatically.',
+    defaultValue: true,
+  },
+  {
+    id: 'delay',
+    description: 'Delay in milliseconds between output clicks.',
+    defaultValue: 75,
+  },
+  {
+    id: 'autoExit',
+    description: 'Automatically exit breach protocol after solving it.',
+    defaultValue: true,
+  },
+  {
+    id: 'soundEnabled',
+    description: 'Disable/enable sound effects.',
+    defaultValue: true,
+  },
+  {
+    id: 'experimentalBufferSizeRecognition',
+    description:
+      'Use experimental buffer size recognition. Recommended for very high in game gamma(above 1.5).',
+    defaultValue: false,
+  },
+  {
+    id: 'format',
+    description:
+      'Format in which save source images. "png" offers better quality, but takes more space on disk.',
+    defaultValue: 'png',
+  },
+  {
+    id: 'historySize',
+    description: 'Amount of history entires saved.',
+    defaultValue: 3,
+  },
+  {
+    id: 'keyBind',
+    description:
+      'Key bind which triggers autosolver. Press "Enter" to save new key bind, or "Escape" to cancel.',
+    defaultValue: 'Alt+`',
+  },
+  {
+    id: 'preserveSourceOnSuccess',
+    description:
+      'Preserve image source after successful breach protocol. This might take a lot of space depending on format and history size options.',
+    defaultValue: true,
+  },
+  {
+    id: 'checkForUpdates',
+    description: 'Check for updates on startup.',
+    defaultValue: true,
+  },
+  {
+    id: 'errorSoundPath',
+    description: 'Path to error sound.',
+    defaultValue: 'C:/Windows/Media/Windows Foreground.wav',
+  },
+  {
+    id: 'thresholdBufferSize',
+    description: 'Fixed threshold value for buffer size fragment.',
+    defaultValue: 127,
+  },
+  {
+    id: 'thresholdBufferSizeAuto',
+    description: 'Use automatic threshold for buffer size fragment.',
+    defaultValue: true,
+  },
+  {
+    id: 'thresholdDaemons',
+    description: 'Fixed threshold value for daemons fragment.',
+    defaultValue: 127,
+  },
+  {
+    id: 'thresholdDaemonsAuto',
+    description: 'Use automatic threshold for daemons fragment.',
+    defaultValue: true,
+  },
+  {
+    id: 'thresholdGrid',
+    description: 'Fixed threshold value for grid fragment.',
+    defaultValue: 127,
+  },
+  {
+    id: 'thresholdGridAuto',
+    description: 'Use automatic threshold for grid fragment.',
+    defaultValue: true,
+  },
+  {
+    id: 'useScaling',
+    description: 'Use Windows scaling to calculate coordinates of squares.',
+    defaultValue: false,
+  },
+  {
+    id: 'minimizeToTray',
+    description: 'Minimize app to system tray.',
+    defaultValue: false,
+  },
+  {
+    id: 'outputDevice',
+    description: 'Output device that will be used to solve breach protocol.',
+    defaultValue: 'mouse',
+  },
 ];
 
-function reduceOptions<T>(
-  options: BreachProtocolOption[],
-  cb: (option: BreachProtocolOption) => T
-) {
-  return options.reduce(
-    (options, option) => ({
-      ...options,
-      [option.name]: cb(option),
-    }),
-    {} as Record<keyof AppSettings, T>
-  );
+function optionsToObject<T>(cb: (option: BreachProtocolOption) => T) {
+  const entries = options.map((o) => [o.id, cb(o)]);
+
+  return Object.fromEntries(entries);
 }
 
-export const defaultOptions = reduceOptions(
-  options,
-  ({ defaultValue }) => defaultValue
-) as AppSettings;
-
-export const optionsDescription = reduceOptions(
-  options,
-  ({ description }) => description
+export const defaultOptions: AppSettings = optionsToObject(
+  (o) => o.defaultValue
 );
+
+export const optionsDescription: Record<keyof AppSettings, string> =
+  optionsToObject((o) => o.description);
