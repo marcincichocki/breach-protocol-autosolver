d501cbc26a65ebdf324207e3addd772d0037b125
Marcin Cichocki <marcincichocki1993@gmail.com>
2021-09-04 20:31:37 +0200

    fix

--- src/electron/worker/sound-player.ts
+++ src/electron/worker/sound-player.ts
@@ -4,15 +4,19 @@ export interface SoundPlayerConfig {
   startSoundPath: string;
 }
 
-export class SoundPlayer {
-  readonly library = new Map([
+interface SoundPlayer {
+  play(id: string): Promise<void>;
+}
+
+export class BreachProtocolSoundPlayer implements SoundPlayer {
+  private readonly library = new Map([
     ['start', this.initAudio(this.config.startSoundPath)],
     ['error', this.initAudio(this.config.errorSoundPath)],
   ]);
 
   constructor(private config: SoundPlayerConfig) {}
 
-  update({ startSoundPath, errorSoundPath }: SoundPlayerConfig) {
+  update({ startSoundPath, errorSoundPath, soundEnabled }: SoundPlayerConfig) {
     if (this.config.startSoundPath !== startSoundPath) {
       this.library.set('start', this.initAudio(startSoundPath));
       this.config.startSoundPath = startSoundPath;
@@ -22,12 +26,14 @@ export class SoundPlayer {
       this.library.set('error', this.initAudio(errorSoundPath));
       this.config.errorSoundPath = errorSoundPath;
     }
+
+    if (this.config.soundEnabled !== soundEnabled) {
+      this.config.soundEnabled === soundEnabled;
+    }
   }
 
   play(id: 'start' | 'error') {
-    if (!this.library.has(id)) {
-      throw new Error(`Provided id: ${id} is invalid`);
-    }
+    if (!this.config.soundEnabled) return;
 
     const audio = this.library.get(id);
 
