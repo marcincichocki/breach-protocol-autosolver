39c0a2fc92261bdfc84035f5c3daba43f2df2753
Marcin Cichocki <marcincichocki1993@gmail.com>
2021-10-12 12:49:27 +0200

    add filtering logic

--- src/core/ocr/base.ts
+++ src/core/ocr/base.ts
@@ -163,6 +163,14 @@ export abstract class BreachProtocolOCRFragment<
     ['ED', 'BD'],
   ]);
 
+  // prettier-ignore
+  private static readonly validCodes = Array
+    .from(BreachProtocolOCRFragment.correctionMap.keys())
+    .concat(HEX_CODES);
+
+  /** Max size in pixels by which height of squares can deviate. */
+  private static readonly maxHeightDeviation = 4;
+
   /** Map containing cropped heights and threshold values. */
   abstract readonly thresholds: Map<number, number>;
 
@@ -196,6 +204,17 @@ export abstract class BreachProtocolOCRFragment<
     return { buffer, source };
   }
 
+  private byCodeHeight(base: number, code: BreachProtocolRecognizerCode) {
+    const height = this.getCodeHeight(code);
+    const diviation = Math.abs(base - height);
+
+    return diviation <= BreachProtocolOCRFragment.maxHeightDeviation;
+  }
+
+  private getCodeHeight({ bbox }: BreachProtocolRecognizerCode) {
+    return bbox.y1 - bbox.y0;
+  }
+
   private codesToSource(
     codes: BreachProtocolRecognizerCode[][]
   ): BreachProtocolSource {
@@ -213,6 +232,30 @@ export abstract class BreachProtocolOCRFragment<
     if (!this.filterRecognizerResults) {
       return this.codesToSource(lines);
     }
+
+    const base = lines
+      .flat()
+      .find((w) => BreachProtocolOCRFragment.validCodes.includes(w.text));
+
+    // There is no valid code, this is most likely not a BP image, or
+    // threshold is completely off target.
+    if (!base) {
+      return this.codesToSource(lines);
+    }
+
+    // Validating by text is bad because some codes can be connected
+    // with each other(in some languages). This could cause false negatives
+    // to be recognized, and would cause lots of issues down the road.
+    //
+    // Validating by code height is better, since font size is constant
+    // across the fragment(max deviation is 2px mostly caused by threshold).
+    // This method doesn't depend on resolution or language.
+    const baseHeight = this.getCodeHeight(base);
+    const filteredLines = lines.map((l) =>
+      l.filter((w) => this.byCodeHeight(baseHeight, w))
+    );
+
+    return this.codesToSource(filteredLines);
   }
 
   protected chunkLine(line: string) {
