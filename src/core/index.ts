18fd8eaee65c13e3288c65a9867f133ee03fc468
Marcin Cichocki <marcincichocki1993@gmail.com>
2022-10-10 18:50:17 +0200

    remove barrel export

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,12 @@
 export * from './common';
 export * from './daemons-i18n';
 export * from './ocr';
-export * from './solver';
+export {
+  BreachProtocol,
+  BreachProtocolStrategy,
+  BreachProtocolResultJSON,
+  BreachProtocolOptions,
+} from './solver/breach-protocol';
+export { FocusHierarchyProvider } from './solver/hierarchy/focus-hierarchy-provider';
+export { IndexHierarchyProvider } from './solver/hierarchy/index-hierarchy-provider';
+export { SequenceJSON } from './solver/sequence';
--- src/core/solver/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './breach-protocol';
-export { FocusHierarchyProvider } from './hierarchy/focus-hierarchy-provider';
-export { IndexHierarchyProvider } from './hierarchy/index-hierarchy-provider';
-export { SequenceJSON } from './sequence';
